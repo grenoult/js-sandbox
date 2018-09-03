b57bd0f53c445df07df3abea969cfe5951ddaf7b
Guillaume Renoult <guillaume.renoult@gmail.com>
2018-09-03 20:05:28 +1000

    Add closure file

---
+++ closure.js
@@ -0,0 +1,46 @@
+/**
+ * Closure is when a function can remember and access its lexical scope even when it's invoked outside its lexical scope.
+ */
+
+// 1 - When calling `calculus()(5,6)`, we calculate using a and b even if they are not in current scope. They are part of calculus() scope.
+function calculus() {
+    let a = 5;
+    let b = 2;
+
+    function doSomeWeirdCalculation(nb1, nb2) {
+        return nb1 * nb2 * a * b;
+    }
+
+    return doSomeWeirdCalculation;
+}
+
+let result = calculus()(5,6);
+
+console.log('1:');
+console.log(result);
+
+
+// 2 - For each iteration, we will have one a new `i` in its own scope.
+// If we use var, it won't create a new variable for each iteration but use the same one.
+
+console.log('2:');
+for (let i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }, 10);
+}
+
+// 3 -- For each iteration, we pass a function to an array that we call later. We then have 10 new scopes and closures.
+
+console.log('3:');
+var functionArray = [];
+for (let i = 0; i < 10; i++) {
+    var newFunction = function() {
+        console.log(i);
+    }
+    functionArray.push(newFunction);
+}
+
+for (var j = 0; j < 10; j++) {
+    functionArray[j]();
+}
